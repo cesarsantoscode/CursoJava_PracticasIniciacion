8f835c7a6510ae6d3acf1614263e2e0418b5e7a2
César Santos <cesarsantostorres@gmail.com>
2022-11-13 18:34:48 -0500

    First commit

---
+++ src/Ejercicio01_01.java
@@ -0,0 +1,7 @@
+public class Ejercicio01_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Estoy aprendiendo a programar bien Java");
+    }
+
+}
---
+++ src/Ejercicio01_02.java
@@ -0,0 +1,8 @@
+public class Ejercicio01_02 {
+
+    public static void main(String[] args) {
+        System.out.println("La vida es tan buena maestra,");
+        System.out.println("que si no aprendes la lección te la repite.");
+    }
+
+}
